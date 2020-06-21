02b2ee3461c228330017c590cfe6e47749adc375
MoizMehmood333 <muhammadmoizmehmood@gmail.com>
2020-06-21 16:19:03 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,376 @@
+
+// ----------------chapter-1------------------
+
+// &&&& task-1 &&&&
+
+// alert("Welcome! to my new website");
+
+// &&&& task-2 &&&&
+
+// alert("Error! Please enter a valid password");
+
+// &&&& task-3 &&&&
+
+// alert("Welcome to JS land... \n Happy Coding!");
+
+// &&&& task-4 &&&&
+
+// alert("Welcome to JS land");
+// alert("happy Coding!")
+
+
+
+// ----------------chapter-2-------------------
+
+// &&&& task-1 &&&&
+
+// var username;
+
+// &&&& task-2 &&&&
+
+// var myName = "Moiz Mehmood";
+// var message = "My full name is: ";
+// alert(message + myName);
+// &&&& task-3 &&&&
+
+// var message = "hello world";
+// alert(message);
+
+
+// &&&& task-4 &&&&
+
+// var studentName = "Moiz Mehmood";
+// var studentAge = "20 years old";
+// var studentProgram = "Certified Mobile application development";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentProgram);
+
+
+// &&&& task-5 &&&&
+
+// var a = ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+// alert(a);
+
+// &&&& task-6 &&&&
+
+// var email = "muhammadmoizmehmood@gmail.com";
+// alert("My email adress is " + email);
+
+
+// &&&& task-7 &&&&
+
+// var book = "A smarter way to learn JavaScript";
+// alert("I'm trying to learn from the Book " + book);
+
+// &&&& task-8 &&&&
+
+// document.write("Yah! I can write html content through javascript.");
+
+// &&&& task-9 &&&&
+
+// alert("“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+
+
+// ----------------chapter-3------------------
+
+// &&&& task-1 &&&&
+
+// var age;
+// var myage = 20;
+// age  = myage;
+// alert("I am " + age + " years old");
+
+// &&&& task-2 &&&&
+
+// var noOfTimesVisited = 18;
+// alert("you have visited this site" + noOfTimesVisited + " times")
+
+
+// &&&& task-3 &&&&
+
+// var birthYear = 2000;
+// document.writeln("My birth year is " + birthYear +"<br> Data type of my  declared varaible is number" );
+
+
+// &&&& task-4 &&&&
+
+// var vistiorsName = "Moiz";
+// var productTitle =  "pants";
+// var quantity = 5;
+// document.writeln( vistiorsName +" ordered " + quantity + " " + productTitle + "on XYZ Clothing store" );
+ 
+// ----------------chapter-4------------------
+
+// &&&& task-1 &&&&
+
+// var a, b, c;
+
+// &&&& task-2 &&&&
+
+// legal variable
+// var age, name, surName, stuName, rollNo;
+
+// illegal variable
+
+// var 1,1ash,%ksdkaslad,,*andlksa,4;
+
+// &&&& task-3 &&&&
+
+// document.writeln("<h1>Rules for naming JS variables <br> </h1>");
+// document.write("Variable names can only contain,$ and _.For example $myvar, _var1 <br>");
+// document.write("Variable must begin with a letter,$ or _.For example $name, _name, name <br>");
+// document.write("Variable names are case sensitive <br>");
+// document.write("Variable names should not be JS Keywords")
+
+
+
+
+// ----------------chapter-5------------------
+
+// &&&& task-1 &&&&
+
+// var num1 = 10, num2 = 15, num;
+// num = num1 + num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + num +"<br>");
+
+
+
+// &&&& task-2 &&&&
+
+// var num1 = 15, num2 = 10, num;
+// num = num1 - num2;
+// document.write("subtraction of " + num1 + " and " + num2 + " is " + num +"<br>");
+
+
+// var num1 = 15, num2 = 10, num;
+// num = num1 * num2;
+// document.write("multiplication of " + num1 + " and " + num2 + " is " + num + "<br>") ;
+
+// var num1 = 15, num2 = 10, num;
+// num = num1 - num2;
+// document.write("division of " + num1 + " by " + num2 + " is " + num +"<br>");
+
+// var num1 = 15, num2 = 10, num;
+// num = num1 % num2;
+// document.write("mod of " + num1 + " to  " + num2 + " is " + num +"<br>");
+
+
+// &&&& task-3 &&&&
+
+// (a)
+
+// var a;
+
+// (b)
+
+// document.write("Value after variable decleration is : " + a + "<br>");
+
+// (c)
+
+// a=5;
+
+// (d)
+
+// document.write("Initial value of varible is : " + a + "<br>");
+
+
+// (e)
+
+// a++;
+
+// (f)
+
+// document.write("Value after increment is: " + a + "<br>");
+
+// (g)
+
+// a = a+7;
+
+// (h)
+
+// document.write("Value after addition is: " + a + "<br>");
+
+// (i)
+
+// a--;
+
+// (j)
+
+// document.write("Value after increment is: " + a + "<br>"); 
+
+// (k)
+
+// a = a % 3;
+// document.write("The remainder is: " + a + "<br>"); 
+
+
+// &&&& task-4 &&&&
+
+// var ticketPrice = 600;
+// var ticketQuantity = 5;
+// var totalTickets = ticketPrice * ticketQuantity;
+// document.write("Total cost to buy " + ticketQuantity + " tickets to a movie is " +  totalTickets + "PKR")
+
+
+// &&&& task-5 &&&&
+
+// var no = 4;
+// document.write("Table of " + no + "<br>")
+// for (let index = 1; index <= 10; index++) {
+    
+//     document.write(no + "x" + index + " = " + no*index +  "<br>" );
+    
+// }
+
+
+// &&&& task-6 &&&&
+
+// var celciusTemp = -40;
+// var convtfarenheit = (celciusTemp * (9/5))+32;
+// document.write(celciusTemp + "C is " +convtfarenheit + "F <br>" );
+
+
+
+// var ferenhietTemp = -40;
+// var convtCelcius = (ferenhietTemp - 32) * (5 / 9);
+// document.write(ferenhietTemp + "F is " + convtCelcius + "C <br>")
+
+// &&&& task-7 &&&&
+
+// var priceItem1 = 400;
+// var priceItem2 = 500;
+// var quantityItem1 = 5;
+// var quantityItem2 = 2;
+
+// document.writeln("Price of item 1 is " + priceItem1 +"<br>");
+// document.writeln("Quantity of item 1 is " + quantityItem1 +"<br>");
+// document.writeln("Price of item 2 is " + priceItem2+"<br>");
+// document.writeln("Quantity of item 2 is " + quantityItem2+"<br>");
+
+// var totalAmount = (priceItem2* quantityItem2)  + (priceItem1*quantityItem1);
+// document.writeln("<br> Total cost of your order is:  " + totalAmount +"<br>");
+
+// &&&& task-8 &&&&
+
+// var obtainedMarks = 680;
+// var totalMarks = 850;
+// var percent = (obtainedMarks / totalMarks) * 100;
+
+// document.writeln("Obtained marks: "+obtainedMarks +"<br>");
+// document.writeln("Total Marks: "+totalMarks +"<br>");
+// document.writeln("Percentage is : "+ percent +"<br>");
+
+
+
+ 
+// &&&& task-9 &&&&
+
+// var usDollar = 104.80;
+// var sadRiyal = 28;
+// var usDollarQuanitity = 10;
+// var sadRiyalQuantity = 25;
+// var totalPakAmount = usDollar*usDollarQuanitity + sadRiyal *sadRiyalQuantity;
+
+// document.writeln("Total Currecy in PKR is : "+ totalPakAmount +"<br>");
+
+// &&&& task-10 &&&&
+
+var num = 110;
+document.writeln("Current value of variable is " + num + ". Value after addtion of 5 is : " + (num= num + 5) + ". value after multiplication by 10 is: " +  (num = num * 10) + ". Value after division by 2 is: " + (num / 2 + "<br>"));
+
+
+// &&&& task-11 &&&&
+
+
+
+// &&&& task-12 &&&&
+
+
+
+// &&&& task-13 &&&&
+
+
+
+// &&&& task-14 &&&&
+
+
+
+// &&&& task-15 &&&&
+
+
+
+// &&&& task-16 &&&&
+
+
+
+
+
+
+
+
+
+// ----------------chapter-------------------
+
+
+// &&&& task-1 &&&&
+
+
+
+// &&&& task-2 &&&&
+
+
+
+// &&&& task-3 &&&&
+
+
+
+// &&&& task-4 &&&&
+
+
+
+// &&&& task-5 &&&&
+
+
+
+// &&&& task-6 &&&&
+
+
+
+// &&&& task-7 &&&&
+
+
+
+// &&&& task-8 &&&&
+
+
+
+// &&&& task-9 &&&&
+
+
+
+// &&&& task-10 &&&&
+
+
+// &&&& task-11 &&&&
+
+
+
+// &&&& task-12 &&&&
+
+
+
+// &&&& task-13 &&&&
+
+
+
+// &&&& task-14 &&&&
+
+
+
+// &&&& task-15 &&&&
+
+
+
+// &&&& task-16 &&&&
